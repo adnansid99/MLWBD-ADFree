eb06253ce447f8428a2942e461bbfd2a4e0cf594
Adnan Siddiqui <asr3012003@gmail.com>
2023-08-28 03:20:56 +0600

    added dialog + some fixes

--- static/js/home.js
+++ static/js/home.js
@@ -14,3 +14,38 @@ const goToBypass = () => {
 const aboutbtn = () => {
   window.open("https://adnan.eu.org", "_blank");
 };
+
+const dialogBtn2 = () => {
+  window.open("https://adnan.eu.org", "_blank");
+};
+
+const dialogBtn1 = () => {
+  const initDate1 = new Date();
+  let savedDate = `${initDate1.getFullYear()}-${(initDate1.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${initDate1.getDate().toString().padStart(2, "0")}`;
+  localStorage.setItem("dialogCheck", savedDate);
+};
+
+const daysDifference = (cD, sD) => {
+  const timeDiff = Math.abs(cD - sD);
+  const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  return daysDiff;
+};
+
+const modalView = () => {
+  dialogView = document.getElementById("dialogView");
+  if (localStorage.getItem("dialogCheck") == null) {
+    dialogView.click();
+  } else {
+    const initDate2 = new Date();
+    let currentDate = `${initDate2.getFullYear()}-${(initDate2.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${initDate2.getDate().toString().padStart(2, "0")}`;
+
+    let savedDate = localStorage.getItem("dialogCheck");
+    if (daysDifference(currentDate, savedDate) == "3") {
+      dialogView.click();
+    }
+  }
+};
