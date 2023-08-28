2d7679f027dbeea87d66a45b837e43eeb4c9bca3
Adnan Siddiqui <asr3012003@gmail.com>
2023-08-28 12:23:17 +0600

    Update domainExt.py

--- src/domainExt.py
+++ src/domainExt.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def domain():
     res = requests.get('https://mlwbd.com')
     soup = BeautifulSoup(res.text, 'html.parser')
-    returnData = soup.find('a', {'class': 'btn-solid-lg page-scroll'})
+    returnData = soup.find('a', {'class': 'btn-solid-lg'})
     domainNamehref = returnData["href"]
     domainSplit = domainNamehref.split(".")
     return domainSplit[1]
